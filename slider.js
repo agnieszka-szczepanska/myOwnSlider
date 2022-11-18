ee7ea3ee44a415611502799fcb74ae0d61671541
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-11-18 11:58:15 +0100

    JS addEventListener added instead of onclick event in html

--- slider.js
+++ slider.js
@@ -1,11 +1,4 @@
 var slider = document.querySelector("#photo");
-var arrowLeft  = document.querySelector("#arrowLeft");
-var arrowRight = document.querySelector("#arrowRight");
-var dot1= document.querySelector("#dot1");
-var dot1= document.querySelector("#dot2");
-var dot1= document.querySelector("#dot3");
-var dot1= document.querySelector("#dot4");
-var dot1= document.querySelector("#dot5");
 var figcaption= document.querySelector("#fotoDescription");
 
 var number = Math.floor(Math.random()*5)+1;
@@ -37,3 +30,20 @@ function previousSlide(){
     }
 
 console.log(number);
+
+arrowLeft.addEventListener("click", previousSlide);
+arrowRight.addEventListener("click", nextSlide);
+
+/* With .onclick you can use one function
+
+for (let i=1; i<=5; i++){
+    document.querySelector("#dot"+i).onclick = function(){
+        chooseSlideNum(i);
+    };
+}
+*/
+
+for (let i=1; i<=5; i++){
+    document.querySelector("#dot"+i).addEventListener("click",function(){
+        chooseSlideNum(i);})
+    };
