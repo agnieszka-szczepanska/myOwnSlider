d3d371d30b669395af41226974325911a693bfb5
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2022-11-21 15:15:47 +0100

    Figcaption is changing while slide is changing by using dots and arrows

--- slider.js
+++ slider.js
@@ -1,51 +1,59 @@
 var slider = document.querySelector("#photo");
-var figcaption= document.querySelector("#fotoDescription");
-
 var number = Math.floor(Math.random()*5)+1;
 var timer1 = 0; /*timer rozpoczynający odliczanie czasu dla bieżącego slide*/
 var timer2 = 0; /*timer rozpoczynający odliczanie czasu do wygaszenia bieżącego slide*/
-var timer3 = 0; /*timer dla figurecaption */
 
-function changeSlide(){
+
+function clearTimers(){
     clearTimeout(timer1);
     clearTimeout(timer2);
+ }
+
+function changeSlide(){
+    clearTimers();
     number++;
     if (number > 5) {number = 1;}
 
     var file = "<img src=\"img/image"+number+".jpg\" id = \"photo\" alt=\"images from Greece\" />";
     slider.innerHTML = file;
     $("#photo").fadeIn(500);
-    document.querySelector('#fotoDescription'+ number).classList.add("active");
-
+    changeFigcaption();
+    
     timer1=setTimeout("changeSlide()" , 3000);
-    setTimeout("changeFigcaption()" , 3000);  /*PRZETESTOWAĆ CZEMU NIE DZIAŁA!!!*/
     timer2=setTimeout("hideSlide()" , 2500);
-
 }
 
+function changeFigcaption(){ 
+    document.querySelector('#fotoDescription'+ number).classList.remove("notActive");
+    document.querySelector('#fotoDescription'+ number).classList.add("active");;
+}; 
+
+
 function chooseSlideNum(num){
-    clearTimeout(timer1);
-    clearTimeout(timer2);
+    clearTimers();
+    hideFigcaption()
     number = num -1;
     changeSlide();
     }
 
 function nextSlide(){
-    clearTimeout(timer1);
-    clearTimeout(timer2);
+    clearTimers();
+    hideFigcaption()
     changeSlide();
 }
-
+ 
 function previousSlide(){
-    clearTimeout(timer1);
-    clearTimeout(timer2);
+    clearTimers();
+    hideFigcaption()
     number=number-1;
+    
     if (number < 1) {number = 5;}
     var file = "<img src=\"img/image"+number+".jpg\" id = \"photo\" alt=\"images from Greece\" />";
     slider.innerHTML = file;
+    changeFigcaption()
     
     timer1=setTimeout("changeSlide()" , 3000);
-    /*tu taka sama nazwa timera, żeby przy wywołaniu innej funkcji się zerował*/
+    timer2=setTimeout("hideSlide()" , 2500);
     }
 
 function hideSlide(){
@@ -53,6 +61,12 @@ function hideSlide(){
     document.querySelector(".active").classList.add("notActive");
     document.querySelector(".active").classList.remove("active");
 }
+
+function hideFigcaption(){
+    document.querySelector(".active").classList.add("notActive");
+    document.querySelector(".active").classList.remove("active");
+}
+ 
 
 arrowLeft.addEventListener("click", previousSlide);
 arrowRight.addEventListener("click", nextSlide);
@@ -73,25 +87,23 @@ for (let i=1; i<=5; i++){
             
 console.log(number);
 
-      /*PRZY PREVIOUS SLIDE, JAK WSKAKUJE NASTĘPNY TO NIE MA FADEIN FACEOUT.... W KOLEJNYCH JUŻ JEST
+    /*
     
     DO ZASTANOWIENIA CZY PRZY DOTS AND ARROWS ZROBIC WYGASZANIE
-    
-    - DODAC ZMIENIAJACE SIE FIGCAPTION
+
+    - kolorowa kropka, gdy jest aktywna
     - RESPONSIVE WEB DESIGN- ZMIANA WIELKOŚCI W ZAL. OD EKRANU ITP*/
+
+
+
 
 /*PRZETESTOWAĆ CZEMU NIE DZIAŁA!
 function changeFigcaption(){
-    var number = 3;
     var activeElement = document.querySelector(".active");
     activeElement.classList.remove("active");
     document.querySelector('#fotoDescription'+ number).classList.add("active");
 };
 */
 
-function changeFigcaption(){   
 
-    document.querySelector('#fotoDescription'+ number).classList.remove("notActive");
-    document.querySelector('#fotoDescription'+ number).classList.add("active");;
-};  
 
